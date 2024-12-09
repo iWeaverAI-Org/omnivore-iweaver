f60c7834e746f1287378f3b37521ce3eb90fe648
xiongjianchuan <3603643@qq.com>
2024-12-09 19:09:04 +0800

    feat: imgproxy

--- packages/api/src/utils/imageproxy.ts
+++ packages/api/src/utils/imageproxy.ts
@@ -13,6 +13,7 @@ export function createImageProxyUrl(
   width = 0,
   height = 0
 ): string {
+  console.log(`createImageProxyUrl ---> ${env.imageProxy.url}`)
   // if (!env.imageProxy.url || !env.imageProxy.secretKey) {
   //   return url
   // }
